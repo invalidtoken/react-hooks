f468b6bfb47d873c9b8297dc5da72cbb9166d518
invalidtoken <codeit.sahil98@gmail.com>
2021-11-30 01:01:21 +0530

    adding pokemon

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -6,7 +6,12 @@ import * as React from 'react'
 // fetchPokemon: the function we call to get the pokemon info
 // PokemonInfoFallback: the thing we show while we're loading the pokemon info
 // PokemonDataView: the stuff we use to display the pokemon info
-import {PokemonForm} from '../pokemon'
+import {
+  fetchPokemon,
+  PokemonDataView,
+  PokemonForm,
+  PokemonInfoFallback,
+} from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
   // 🐨 Have state for the pokemon (null)
@@ -23,6 +28,22 @@ function PokemonInfo({pokemonName}) {
   //   1. no pokemonName: 'Submit a pokemon'
   //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
   //   3. pokemon: <PokemonDataView pokemon={pokemon} />
+
+  const [loading, setLoading] = React.useState(false)
+  const [pokemonData, setPokemonData] = React.useState(null)
+
+  React.useEffect(() => {
+    setLoading(true)
+    fetchPokemon(pokemonName).then(pokemonData => {
+      setLoading(false)
+      setPokemonData(pokemonData)
+    })
+  }, [pokemonName])
+
+  if (!pokemonName) return 'Submit a pokemon'
+
+  if (loading) return <PokemonInfoFallback name={pokemonName} />
+  if (pokemonData) return <PokemonDataView pokemon={pokemonData} />
 
   // 💣 remove this
   return 'TODO'
