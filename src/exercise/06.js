93b97eca6d7a7bbb0b060f63fc2a95522b0e2090
invalidtoken <codeit.sahil98@gmail.com>
2021-11-30 15:03:15 +0530

    adding pokemong

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -2,51 +2,52 @@
 // http://localhost:3000/isolated/exercise/06.js
 
 import * as React from 'react'
-// 🐨 you'll want the following additional things from '../pokemon':
-// fetchPokemon: the function we call to get the pokemon info
-// PokemonInfoFallback: the thing we show while we're loading the pokemon info
-// PokemonDataView: the stuff we use to display the pokemon info
+
 import {
   fetchPokemon,
   PokemonDataView,
+  PokemonErrorBoundary,
   PokemonForm,
   PokemonInfoFallback,
 } from '../pokemon'
 
 function PokemonInfo({pokemonName}) {
-  // 🐨 Have state for the pokemon (null)
-  // 🐨 use React.useEffect where the callback should be called whenever the
-  // pokemon name changes.
-  // 💰 DON'T FORGET THE DEPENDENCIES ARRAY!
-  // 💰 if the pokemonName is falsy (an empty string) then don't bother making the request (exit early).
-  // 🐨 before calling `fetchPokemon`, clear the current pokemon state by setting it to null
-  // 💰 Use the `fetchPokemon` function to fetch a pokemon by its name:
-  //   fetchPokemon('Pikachu').then(
-  //     pokemonData => {/* update all the state here */},
-  //   )
-  // 🐨 return the following things based on the `pokemon` state and `pokemonName` prop:
-  //   1. no pokemonName: 'Submit a pokemon'
-  //   2. pokemonName but no pokemon: <PokemonInfoFallback name={pokemonName} />
-  //   3. pokemon: <PokemonDataView pokemon={pokemon} />
-
-  const [loading, setLoading] = React.useState(false)
   const [pokemonData, setPokemonData] = React.useState(null)
+  const [error, setError] = React.useState(null)
 
   React.useEffect(() => {
-    setLoading(true)
-    fetchPokemon(pokemonName).then(pokemonData => {
-      setLoading(false)
-      setPokemonData(pokemonData)
-    })
+    if (!pokemonName) {
+      return
+    }
+
+    setError(null)
+    setPokemonData(null)
+    fetchPokemon(pokemonName).then(
+      pokemonData => {
+        setError('sahil')
+        setPokemonData(pokemonData)
+      },
+      error => setError(error),
+    )
   }, [pokemonName])
 
+  console.log('pokemon data', pokemonData)
+  console.log('error ', error)
+
   if (!pokemonName) return 'Submit a pokemon'
+  if (error)
+    return (
+      <div role="alert">
+        There was an error:
+        <pre style={{whiteSpace: 'normal'}}>{error.message}</pre>
+      </div>
+    )
 
-  if (loading) return <PokemonInfoFallback name={pokemonName} />
-  if (pokemonData) return <PokemonDataView pokemon={pokemonData} />
-
-  // 💣 remove this
-  return 'TODO'
+  if (pokemonData) {
+    return <PokemonDataView pokemon={pokemonData} />
+  } else {
+    return <PokemonInfoFallback name={pokemonName} />
+  }
 }
 
 function App() {
